2114bacc43db7242ce511b4b298c8d03409e1e6b
Malaxor <nogobaduk@gmail.com>
2020-09-16 13:45:06 -0400

    User model, express-validator

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const UserSchema = new Schema ({
+   name: {
+      type: String,
+      trim: true,
+      required: [true, 'Name is required.']
+   },
+   email: {
+      type: String,
+      required: [true, 'Email is required.'],
+      unique: true
+   },
+   password: {
+      type: String,
+      required: [true, 'Password is required.']
+   },
+   avatar: String,
+   date: {
+      type: Date,
+      default: Date.now
+   }
+});
+mongoose.model('User', UserSchema);
