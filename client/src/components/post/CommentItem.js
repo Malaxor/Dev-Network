8dd4698d6d0efbab95f66de0a09c8d21deadf73c
Malaxor <nogobaduk@gmail.com>
2020-12-01 15:20:59 -0500

    comment item component wokring

---
+++ client/src/components/post/CommentItem.js
@@ -0,0 +1,46 @@
+import React, { Fragment } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Moment from 'react-moment';
+import { deleteComment } from '../../actions/post';
+
+const CommentItem = ({ 
+   postId, 
+   comment: { _id, user, avatar, author, content, date },
+   auth,
+   deleteComment
+}) => (
+   <div class="post bg--white p-16 my-16">
+      <div>
+        <Link to={`/profile/${user}`}>
+          <img
+            class="round-img"
+            src={avatar}
+            alt=""
+          />
+          <h4>{author}</h4>
+        </Link>
+      </div>
+      <div>
+        <p class="my-16">
+           {content}
+        </p>
+         <p class="post-date">
+            Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
+        </p>
+         {
+            !auth.loading && user === auth.user._id && (
+               <button 
+                  className='btn btn--danger' 
+                  onClick={e => deleteComment(postId, _id)}
+               >
+                  <i className="fas fa-times"></i>
+               </button>
+            )
+         }
+      </div>
+   </div>
+)
+
+const mapStateToProps = state => ({ auth: state.auth });
+export default connect(mapStateToProps,{ deleteComment })(CommentItem);
--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import Spinner from '../layout/Spinner';
 import PostItem from '../posts/PostItem';
 import CommentForm from '../post/CommentForm';
+import CommentItem from '../post/CommentItem';
 import { getPost } from '../../actions/post';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
@@ -16,6 +17,11 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
       <Link to='/posts' className='btn'>Back to Posts</Link>
       <PostItem post={post} displayBtns={false} />
       <CommentForm postId={post._id} />
+      <div className="comments">
+         {post.comments.map(comment => 
+            <CommentItem key={comment._id} comment={comment} postId={post._id} />
+         )}
+      </div>
    </Fragment>
 }
 const mapStateToProps = state => ({
